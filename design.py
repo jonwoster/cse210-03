8cd0ee527c6e9ef8b1040d1cc3b83e738ee44b13
Jonathan Woster <jonathanwoster@gmail.com>
2022-02-04 11:59:18 -0600

    Edits to the design- Jonathan

--- design.py
+++ design.py
@@ -1,41 +1,47 @@
-# The program must include a README file.  Done
-
-The parachute is part of a list with 5 strings in it 
-the person is also in the list
-
+"""
+The program must include a README file.
+The program must remain true to game play described in the overview.
 The program must include class and method comments.
-
-
 The program must have at least four classes.
 
-    director class
-        start_game
-        call secret_word_generator class
-        capture_guess by using terminal service class to ask user
-        do_update
-            evaluate guess
-        display by using terminal service class
+The parachute will be part of a list with 5 strings in it 
+The person is also in the list and is made of 3 strings
 
-    word_and_parachute class
-        keep track of pieces left in parachute, updates the list with the strings
-        keep track of what has been guessed correctly, updates the string 
+Will use encapsulation by using "_" in front of attribute names (such as self._attribute) 
+    to prevent classes from changing each others attributes directly. Data passed into 
+    and out of classes to be done using arguments and return statements.
+
+__main__.py will be in jumper folder, will call the Director class
+jumper folder will have a game folder
+game folder will have:
+    director.py (with Director class in it)
+    evaluate_guess.py (with Evaluate_guess class in it)
+    terminal_service.py (with Terminal_service class in it)
+    word_generator.py (with Word_generator class in it)
+
+Director class
+    start_game method
+    initialize attributes needed for this class
+    secret_word method which will call Word_generator class to get secret word
+    capture_guess method which will use Terminal_service class to ask user for their guess
+    do_update method will use Evaluate_guess class
+    display_status method which will use Terminal_service class to show the 
+        remaining blanks and any correctly guessed letters
+        what is left of the parachute and the person
+
+Evaluate_guess class
+    Gets the guess and secret word from Director via arguments
+    Checks the guess against the secret word
+    Updates the string to remove lines from the parachute list if needed
+    Updates the word to shows proper spaces and letters
+    Returns updated list for parachute and returns the updated string for the word
     
-    terminal_service class
-        input and output, like seeker game
+Terminal_service class
+    set it up like input and output from seeker game
+    * Not sure how it will handle multi-line strings like parachute & the person?
     
-    secret_word_generator class
-            initialize with list of word
-            pick word
+Word_generator class
+    initialize with list of words
+    generate_word method to pick secret word 
 
-    
-
-
-get guess from user
-cut parachute
-keep playing decision (did they win, did they lose parachute)
-adds correct letter to the display for player to see
-    
-
-
-
-The program must remain true to game play described in the overview.
+"""
