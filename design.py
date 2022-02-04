ab1b17b3da4bc52ef8bb40ea9a58dbfb6057b732
Jonathan Woster <jonathanwoster@gmail.com>
2022-02-03 21:10:38 -0600

    update after design session

---
+++ design.py
@@ -0,0 +1,41 @@
+# The program must include a README file.  Done
+
+The parachute is part of a list with 5 strings in it 
+the person is also in the list
+
+The program must include class and method comments.
+
+
+The program must have at least four classes.
+
+    director class
+        start_game
+        call secret_word_generator class
+        capture_guess by using terminal service class to ask user
+        do_update
+            evaluate guess
+        display by using terminal service class
+
+    word_and_parachute class
+        keep track of pieces left in parachute, updates the list with the strings
+        keep track of what has been guessed correctly, updates the string 
+    
+    terminal_service class
+        input and output, like seeker game
+    
+    secret_word_generator class
+            initialize with list of word
+            pick word
+
+    
+
+
+get guess from user
+cut parachute
+keep playing decision (did they win, did they lose parachute)
+adds correct letter to the display for player to see
+    
+
+
+
+The program must remain true to game play described in the overview.
