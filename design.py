e86b1309d8da744e5266c31ee7ce5f54c736ddb8
Jonathan Woster <jonathanwoster@gmail.com>
2022-02-06 19:43:42 -0600

    Updates to the design

--- design.py
+++ design.py
@@ -11,37 +11,51 @@ Will use encapsulation by using "_" in front of attribute names (such as self._a
     to prevent classes from changing each others attributes directly. Data passed into 
     and out of classes to be done using arguments and return statements.
 
-__main__.py will be in jumper folder, will call the Director class
+__main__.py will be in jumper folder, will call the Director class -Jonathan to create main
 jumper folder will have a game folder
 game folder will have:
-    director.py (with Director class in it)
-    evaluate_guess.py (with Evaluate_guess class in it)
-    terminal_service.py (with Terminal_service class in it)
-    word_generator.py (with Word_generator class in it)
+    director.py (with Director class in it)  -Camron to create the file
+    evaluate_guess.py (with Evaluate_guess class in it)  -Jonathan to create the file
+    terminal_service.py (with Terminal_service class in it)  -Arnaldo to create the file
+    word_generator.py (with Word_generator class in it) -Monika to create the file
 
 Director class
-    start_game method
-    initialize attributes needed for this class
-    secret_word method which will call Word_generator class to get secret word
-    capture_guess method which will use Terminal_service class to ask user for their guess
-    do_update method will use Evaluate_guess class
-    display_status method which will use Terminal_service class to show the 
+    
+    Director attributes: - Camron
+    self._secret_word = WordGenerator().create_word()
+    self._is_playing = True
+    self._terminal_service = TerminalService()
+
+     Methods:
+    start_game(self)  - Camron
+	(loop through methods)
+  
+    secret_word method which will call Word_generator class to get secret word-  Monika
+
+    capture_guess(self)  - Arnaldo
+	    guess = self._terminal_service.read_guess()
+
+    do_update method will use Evaluate_guess class  - Camron
+
+    display_status method which will use Terminal_service class to show the - Arnaldo
         remaining blanks and any correctly guessed letters
         what is left of the parachute and the person
 
-Evaluate_guess class
+Evaluate_guess class - Jonathan
     Gets the guess and secret word from Director via arguments
     Checks the guess against the secret word
     Updates the string to remove lines from the parachute list if needed
     Updates the word to shows proper spaces and letters
     Returns updated list for parachute and returns the updated string for the word
     
-Terminal_service class
+Terminal_service class - Arnaldo
     set it up like input and output from seeker game
     * Not sure how it will handle multi-line strings like parachute & the person?
     
-Word_generator class
-    initialize with list of words
-    generate_word method to pick secret word 
+Word_generator class  -Monika
+    self._words = [*this will be the list of words Monika has]
+    generate_word(self)
+	    word_index = random.randint(1,len(self.words))
+	    return self.words[word_index]
 
 """
