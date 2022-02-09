201bff4e36c119ea5e6fa9a37c8c51c995ddb747
nikasparks <nikasparks@mac.com>
2022-02-09 05:56:05 -0700

    Major update. Display letters and Jumper correctly

--- jumper/game/director.py
+++ jumper/game/director.py
@@ -9,34 +9,52 @@ class Director:
         self._is_playing = True
         self._terminal_service = Terminal_service()
         self._evaluate_guess = Evaluate_guess()
-        self._secret_word = ""
+        self._secret_word = Word_Generator().generate_word()
         #could be assigned here or in the method
         self._user_guess = ""
+        self._hidden_word ="_"* len(self._secret_word)
+        
         #update through capture_guess?
 
     def start_game(self):
         """starts and runs the main game loop until 'self._is_playing' becomes false."""
         while self._is_playing:
             self.display_status()
-            self.capture_guess()
             self.do_update()
+            # self.capture_guess()
             
-    def secret_word(self):
-        word = Word_Generator()
-        self._secret_word = word.generate_word()
+    # def secret_word(self):
+    #     word = Word_Generator()
+    #     self._secret_word = word.generate_word()
     
     def display_status(self):
+        
+        self._show_word = self._evaluate_guess.get_word_printout(self._secret_word, self._user_guess, self._hidden_word)
+        self._terminal_service.write_text(f"\n{self._show_word}\n")
         self._image = self._evaluate_guess.get_jumper_image(self._user_guess, self._secret_word)
-        self._terminal_service.write_text(self._image)
-    
+        self._terminal_service.write_text(f"{self._image}")
+        self._hidden_word = self._show_word
+
     def capture_guess(self):
         self._user_guess = self._terminal_service.read_letter("Guess a letter [a-z]: ")
     
     def do_update(self):
         """Sends guess info to 'Evaluate_guess and recieves the updated image and word string. Then sends results to 'Terminal_service' to be displayed."""
-        guess = Evaluate_guess()
-        img = guess.get_jumper_image(self._user_guess, self._secret_word)
-        word_print = guess.get_word_printout()
+        # guess = Evaluate_guess()
+        if '_' not in self._hidden_word:
+            self._win = self._terminal_service.write_text(f"\nYou found the word: {self._secret_word}.\nThe jumper has landed safely!\nThanks for playing!\n")
+            self._is_playing = False
+            
+        elif 'x' in self._image:
+            self._lose = self._terminal_service.write_text(f"\nThe word was {self._secret_word}.\nThe jumper did not land safely!\nGame Over.\nThanks for playing!\n")
+            self._is_playing = False
+            
+            
+        else:
+            self._is_playing = True
+            self.capture_guess()
+        # img = guess.get_jumper_image(self._user_guess, self._secret_word)
+        # word_print = guess.get_word_printout()
         #will need to send this to the terminal_service
 
     
--- jumper/game/evaluate_guess.py
+++ jumper/game/evaluate_guess.py
@@ -21,10 +21,11 @@ class Evaluate_guess:
             self (Seeker): An instance of Evaluate_guess
         """
         # list of strings to represent the parachute and jumper
-        self._jumper_image = ["  ___", " /   \\", "  ___", " \\   / ", "  \\ / ", '   O', "  /|\\", "  / \\", " ", "~~~~~~~~" ] 
+        self._jumper_image = ["  ___", " /___\\", " \\   / ", "  \\ / ", '   O', "  /|\\", "  / \\", " ", "~~~~~~~~" ] 
 
-        self._word_printout = ["- - - - - - word guessed so far will go here"] 
-    
+        
+        
+
     def get_jumper_image(self, user_guess, secret_word):
         """Receives the guess and the secret word from Director class
             Checks the guess against the secret word
@@ -35,32 +36,59 @@ class Evaluate_guess:
         Returns:
             list: the parachute and jumper strings (the GUI representation)
         """
+        self._picture = ""
         self._guess = user_guess.lower()  # make sure the guess is lower case
         self._secret_word = secret_word.lower()  # make sure the secret word is lower case
         
-        # Count how many times the guessed letter is in the secret word
-        self._occurences = self._secret_word.count(self._guess)
+        if self._jumper_image[0] != '   x':
+            # Count how many times the guessed letter is in the secret word
+            self._occurences = self._secret_word.count(self._guess)
 
-        # if the letter that was guessed is not in self._secret_word
-        if self._occurences == 0:
-            # then remove the first item in self._jumper_image
-            self._jumper_image.pop(0)
+            if self._occurences == 0 and len(self._jumper_image) == 6:
+                self._jumper_image.pop(0)
+                self._jumper_image.pop(0)
+                self._jumper_image.insert(0,'   x')
 
-         # if the letter that was guessed is in self._secret_word then loop through to replace blanks with letters
-        elif self._occurences > 0:
-            # then update the self._word_printout
-            list_of_occurences = []
-            for pos,char in enumerate(self._secret_word):
-                if(char == self._guess):
-                    list_of_occurences.append(pos)
+            # if the letter that was guessed is not in self._secret_word
+            elif self._occurences == 0:
+                # then remove the first item in self._jumper_image
+                self._jumper_image.pop(0)
 
+            # if the letter that was guessed is in self._secret_word then loop through to replace blanks with letters
+            # elif self._occurences > 0:
+            #     # then update the self._word_printout
+            #     list_of_occurences = []
+            #     for pos,char in enumerate(self._secret_word):
+            #         if(char == self._guess):
+            #             list_of_occurences.append(pos)
+
+        for i in self._jumper_image:
+            self._picture += i +'\n'
 
         # print(f"Debugging: jumper image from within Evaluate_guess is {self._jumper_image}") # for debugging
         # send the jumper image list of strings back to the calling function/class
-        return self._jumper_image
+        return self._picture
 
-    def get_word_printout(self):
+    def get_word_printout(self, secret_word, guess, hidden_word):
         # stuff goes here
+        self._word_printout = "" 
+        self.letter_list = list(hidden_word.replace(" ",""))
+        """Checks the word to see is the letter guessed is in the word."""
+
         
+        if guess in secret_word:
+            for i in range(0,len(secret_word)):
+                letter = secret_word[i]
+                if guess == letter:
+                    self.letter_list.insert(i, guess)
+                    self.letter_list.pop(i+1)
+                # elif self.letter_list[i] == :
+                #     self.letter_list.append("-")
+        for i in self.letter_list:
+            self._word_printout += i + " "
+
+
+
+
         # print(f"Debugging: word_printout from wihtin get_word_printout is {self._word_printout}") # for debugging
         return self._word_printout
